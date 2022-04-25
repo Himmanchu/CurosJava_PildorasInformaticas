abefc921c2459bfbfcef11def646382f52187efc
Himmanchu <104020906+Himmanchu@users.noreply.github.com>
2022-04-25 11:37:36 +0200

    Actualizacion1

---
+++ src/Calculos_conMath.java
@@ -0,0 +1,26 @@
+
+public class Calculos_conMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//double raiz=Math.sqrt(9);
+		
+		float num1=5.85f;
+		
+		int resultado=Math.round(num1);
+		
+		System.out.println(resultado);
+		
+		double base=6;
+		
+		double exponente=3;
+		
+		double resultado2=Math.pow(base, exponente);
+		
+		System.out.print("El resultado de "+ base + " elevado a " + exponente + " es " +resultado2);
+		
+	}
+
+}
