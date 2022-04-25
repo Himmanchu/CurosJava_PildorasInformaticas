01b74685c5a15b9832b26c1f3b623fd4fd9a5d8b
Himmanchu <104020906+Himmanchu@users.noreply.github.com>
2022-04-25 11:14:12 +0200

    Initial commit

---
+++ src/Declaraciones_operadores.java
@@ -0,0 +1,39 @@
+
+public class Declaraciones_operadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//int a=5;
+		//int b=7;
+
+		//int c=b+a;
+		
+		//c+=6;
+		
+		//double a=5;
+		//double b=7;
+		//double c=b/a;
+		
+		final double apulgadas=2.54;
+		
+		double cm=6;
+		
+		double resultado=cm/apulgadas;
+		
+		
+		System.out.println("En " + cm + " cm hay " + resultado + " pulgadas.");
+		
+		
+		
+		int operador1, operador2, resultado2;
+		
+		operador1= 8;
+		operador2= 7;
+		
+		resultado2= operador1+operador2;
+		System.out.println(resultado2);
+		
+	}
+
+}
---
+++ src/Variables.java
@@ -0,0 +1,21 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//forma de hacer comentarios en una línea
+		/*comentarios en 2 líneas
+		 * de esta manera
+		 */
+			int	edad=35;
+			
+			
+			
+			System.out.println(edad);
+			
+					
+	}
+
+}
