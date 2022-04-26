c3dd9ddd6c1fcc95b13407e256a246d01acf8680
Himmanchu <104020906+Himmanchu@users.noreply.github.com>
2022-04-26 10:42:37 +0200

    Commit

---
+++ src/Acceso_aplicación.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+
+public class Acceso_aplicación {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String clave="Himman";
+		
+		String pass="";
+		
+		while(clave.equals(pass)==false) {
+			
+			pass=JOptionPane.showInputDialog("Introduce la contraseña, por favor");
+			
+			if(clave.equals(pass)==false) {
+				
+				System.out.println("Contraseña incorrecta");
+				
+			}
+		}
+		
+		System.out.println("Contraseña correcta. Acceso permitido");
+	}
+
+}
