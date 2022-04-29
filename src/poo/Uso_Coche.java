ac5cc7f6f6011e5d316714e9e6cb708e6d1149a3
Himmanchu <104020906+Himmanchu@users.noreply.github.com>
2022-04-29 12:56:38 +0200

    Commit

---
+++ src/poo/Uso_Coche.java
@@ -0,0 +1,15 @@
+package poo;
+
+public class Uso_Coche {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Coche Renault=new Coche();  //INSTANCIAR UNA CLASE. EJEMPLAR UNA CLASE
+		
+		System.out.println("Este coche tiene " + Renault.ruedas + " ruedas.");
+		
+		
+	} 
+
+}
