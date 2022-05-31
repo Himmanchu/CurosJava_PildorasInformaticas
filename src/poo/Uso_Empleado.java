29ef13d86b493e27dee92b714070c3eb313137a2
Himmanchu <104020906+Himmanchu@users.noreply.github.com>
2022-05-31 11:39:13 +0200

    Commit

--- src/poo/Uso_Empleado.java
+++ src/poo/Uso_Empleado.java
@@ -122,3 +122,31 @@ class Empleado{
 	
 	
 }
+
+
+class Jefatura extends Empleado{
+	
+	public Jefatura(String nom, double sue, int agno, int mes, int dia) {
+		
+		super(nom, sue, agno, mes, dia);
+		
+	}
+
+		public void estableceIncentivo(double b) {
+			
+			incentivo=b;
+		}
+	
+		public double dameSueldo() {
+			
+			double sueldoJefe=dameSueldo();
+			
+			return sueldoJefe + incentivo;
+		}
+		
+	private double incentivo;
+	
+	
+	
+	
+}
