a8f8904ad3739b7ff816c833567ccceb385ab2c5
Himmanchu <104020906+Himmanchu@users.noreply.github.com>
2022-06-02 11:55:10 +0200

    Commit

---
+++ src/poo/Uso_Persona.java
@@ -0,0 +1,69 @@
+package poo;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Uso_Persona {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		
+		
+	}
+
+}
+
+
+class Empleado2{    
+	
+	public Empleado2(String nom, double sue, int agno, int mes, int dia) {
+		
+		//método constructor tiene que tener el mismo nombre que la clase a la que pertenece
+	
+		
+		sueldo=sue;
+		
+		GregorianCalendar calendario=new GregorianCalendar(agno, mes-1, dia);
+		
+		altaContrato=calendario.getTime();
+		
+		++IdSiguiente;
+		
+		Id=IdSiguiente;
+		
+	}
+	
+
+	public double dameSueldo() {    //getter
+		
+		return sueldo;
+	}
+	
+	
+	public Date dameFechaContrato() {    //getter
+		
+		return altaContrato;
+	}
+	
+	public void subeSueldo(double porcentaje) {    //setter
+		
+		double aumento=sueldo*porcentaje/100;
+		
+		sueldo+=aumento;
+	}
+	
+	
+	private double sueldo;
+	
+	private Date altaContrato;
+	
+	private static int IdSiguiente;
+	
+	private int Id;
+	
+	
+}
