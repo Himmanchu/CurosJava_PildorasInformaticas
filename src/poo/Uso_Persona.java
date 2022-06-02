8dc5c4d7dcc5ea6553b5210b4057d834e451525c
Himmanchu <104020906+Himmanchu@users.noreply.github.com>
2022-06-02 12:15:30 +0200

    Commit

--- src/poo/Uso_Persona.java
+++ src/poo/Uso_Persona.java
@@ -8,7 +8,18 @@ public class Uso_Persona {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-
+		
+		Persona[] lasPersonas=new Persona[2];
+		
+		lasPersonas[0]=new Empleado2("Luis Conde", 50000, 2009, 02, 25);
+		
+		lasPersonas[1]=new Alumno("Ana López", "Biológicas");
+		
+		
+		for(Persona p: lasPersonas) {
+			
+			System.out.println(p.dameNombre() + ", " + p.dameDescripcion());
+		}
 		
 	}
 
@@ -35,12 +46,13 @@ abstract class Persona{
 	
 }
 
-class Empleado2{    
+class Empleado2 extends Persona{    
 	
 	public Empleado2(String nom, double sue, int agno, int mes, int dia) {
 		
 		//método constructor tiene que tener el mismo nombre que la clase a la que pertenece
 	
+		super(nom);
 		
 		sueldo=sue;
 		
@@ -51,6 +63,12 @@ class Empleado2{
 		++IdSiguiente;
 		
 		Id=IdSiguiente;
+		
+	}
+	
+	public String dameDescripcion() {
+		
+		return "Este empleado tiene un Id= " + Id + " con un sueldo= " + sueldo;
 		
 	}
 	
@@ -82,5 +100,26 @@ class Empleado2{
 	
 	private int Id;
 	
+	
+}
+
+
+class Alumno extends Persona{
+	
+	public Alumno(String nom, String car) {
+		
+		super(nom);
+		
+		carrera=car;
+		
+	}
+	
+	public String dameDescripcion() {
+		
+		return "Este alumno está estudiando la carrera de " + carrera;
+		
+	}
+	
+	private String carrera;
 	
 }
